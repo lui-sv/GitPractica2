7be7550195f64bf16605be7978066bd1214bbd60
Luis <Luis@DESKTOP-NT9I88C>
2024-03-05 13:14:57 -0500

    Nueva clase agregada

---
+++ src/gitpractica2/pruebaa.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gitpractica2;
+
+/**
+ *
+ * @author Luis
+ */
+public class pruebaa {
+    String Nombre;
+    String Apellido;
+}
