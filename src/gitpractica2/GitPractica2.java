fb5cc67272843cb6e287db4a2626549a3f77ca22
Luis <Luis@DESKTOP-NT9I88C>
2024-03-05 13:36:07 -0500

    codigo hola mundo

--- src/gitpractica2/GitPractica2.java
+++ src/gitpractica2/GitPractica2.java
@@ -14,7 +14,10 @@ public class GitPractica2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World!");
     }
-    
 }
+
+    
+    
+
